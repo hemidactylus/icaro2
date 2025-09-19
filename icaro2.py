a3c07c3f303e76d1c10096114d791ab79afeb46c
Stefano Lottini <stefano.lottini@datastax.com>
2025-09-20 00:46:07 +0200

    context-rich next-wall strategies

--- icaro2.py
+++ icaro2.py
@@ -2,9 +2,11 @@ import math
 import random
 from functools import lru_cache
 
-N = 16
-M = 12
+N = 24
+M = 18
 lmb = 0.2
+lmb2 = 0.6
+lmb3 = 2.5
 
 def zero(n,m):
     return [
@@ -61,39 +63,44 @@ def saturate(l):
         l[i][j][z] = 1
 
 
-def has_walls(i, j, l):
-    # is the corner at i, j touched by some walls? 
-    full = l[i][j][0] + l[i][j][1] + l[i-1][j][0] + l[i][j-1][1]
-    return full > 0
+def survey_walls(i, j, l):
+    # count walls entering either ends of this proposed wall
+    vs = l[i][j][1] + l[i][j-1][1]
+    hs = l[i][j][0] + l[i-1][j][0]
+    return (hs, vs)
 
 
 def eligible_wall_pos(pos, l):
+    # return bool, (edge counts h/v)
     i,j,z=pos
     # must be empty
     if l[i][j][z] == 1:
-        return False
+        return False, (0,0)
     # one end must bear wall, the other no
     end0 = (i, j)
     end1 = (i + (1 - z), j + z)
-    has_walls0 = has_walls(*end0, l)
-    has_walls1 = has_walls(*end1, l)
-    return has_walls0 ^ has_walls1
+    ws0 = survey_walls(*end0, l)
+    ws1 = survey_walls(*end1, l)
+    return (ws0[0]+ws0[1] > 0) ^ (ws1[0]+ws1[1] > 0), (ws0[0]+ws1[0], ws0[1]+ws1[1])
 
 
 @lru_cache
-def wall_weight(i, j, hn, hm, lmb):
-    return math.exp(-lmb*(math.fabs(i-hn) + math.fabs(j-hm)))
+def wall_weight(i, j, n, m, lmb, z, wshs):
+    # prefer central (lmb)
+    # prefer corners over straight long segments (lmb2)
+    # prefer walls not starting off boundaries (lmb3)
+    isborder = 1 if i==0 or j==0 or i==n-1 or j==m-1 else 0
+    hn= 0.5 * n
+    hm= 0.5 * m
+    return math.exp(-lmb*(math.fabs(i-hn) + math.fabs(j-hm)) -lmb2*(wshs[z]-wshs[1-z]) - lmb3*isborder)
 
 def choose_wall(cand_walls, l):
-    # uniform
-    # return cand_walls[random.randint(0, len(cand_walls)-1)]
+    n=len(l) - 1
+    m=len(l[0]) - 1
 
-    # prefer central
-    hn=0.5 * (len(l) - 1)
-    hm=0.5 * (len(l[0]) - 1)
     weights = [
-        wall_weight(i, j, hn, hm, lmb)
-        for i, j, _ in cand_walls
+        wall_weight(i, j, n, m, lmb, z, wshs)
+        for (i, j, z), (elib, wshs) in cand_walls
     ]
     cumw = [
         sum(weights[0:p+1])
@@ -111,15 +118,15 @@ def add_one(l):
     n=len(l) - 1
     m=len(l[0]) - 1
     cand_wallpos = [
-        cand_pos
+        (cand_pos, eli)
         for cand_pos in candidate_pos(n,m)
-        if eligible_wall_pos(cand_pos, l)
+        if (eli := eligible_wall_pos(cand_pos, l))[0]
     ]
     if cand_wallpos == []:
         return False
 
     wall_pos = choose_wall(cand_wallpos, l)
-    i,j,z=wall_pos
+    i,j,z=wall_pos[0]
     l[i][j][z]=1
     return True
 
